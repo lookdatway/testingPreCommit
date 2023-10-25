a204208263559687890ef11c3844c16b517cc1d4
Toby Cook <tobyracook@gmail.com>
2023-10-25 13:57:15 +1030

    adding files

---
+++ test_birthday.py
@@ -0,0 +1,19 @@
+from birthday import *   # The code to test
+import pytest
+
+@pytest.fixture
+def exampleBirthday():
+    data =   {
+        'yyyy': 2002,
+        'mm':11,
+        'dd':23
+    }
+    return data
+    
+   
+
+def test_birthday(exampleBirthday):
+    assert get_age(exampleBirthday['yyyy'],exampleBirthday['mm'],exampleBirthday['dd']) >20 and  get_age(exampleBirthday['yyyy'],exampleBirthday['mm'],exampleBirthday['dd'])< 21
+
+def test_sums(exampleBirthday):
+    assert sum(exampleBirthday['yyyy'],exampleBirthday['mm'],exampleBirthday['dd'] ) == 2036
