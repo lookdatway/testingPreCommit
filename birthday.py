2e41feb19d43c67a49fdd55d89352f1ab34c69a2
Toby Cook <tobyracook@gmail.com>
2023-10-25 14:35:30 +1030

    test

--- birthday.py
+++ birthday.py
@@ -1,11 +1,13 @@
 import datetime
 
-def get_age(yyyy:int, mm:int, dd:int) -> int: 
+
+def get_age(yyyy: int, mm: int, dd: int) -> int:
     dob = datetime.date(yyyy, mm, dd)
     today = datetime.date.today()
-    age = ((today - dob).days / 365.25)
+    age = (today - dob).days / 365.25
     return age
 
-def sum(yyyy:int, mm:int, dd:int) -> int: 
-    
-    return yyyy+mm+dd
+
+def sum(yyyy: int, mm: int, dd: int) -> int:
+
+    return yyyy + mm + dd
